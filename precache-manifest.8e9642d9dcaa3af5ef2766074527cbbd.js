886904d1d4d59e64edcd17810c3d689f0bdbfae8
KyleBlackie <ian921@bell.net>
2019-08-20 13:09:31 -0400

    Updates

---
+++ precache-manifest.8e9642d9dcaa3af5ef2766074527cbbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805aeb405a07646ad68b7e19e7dbac82",
+    "url": "/productivity-app/index.html"
+  },
+  {
+    "revision": "d764ec9bdc86ba26aadb",
+    "url": "/productivity-app/static/css/main.32be136d.chunk.css"
+  },
+  {
+    "revision": "97ffc77b1c9a7d4173b2",
+    "url": "/productivity-app/static/js/2.e2d88e5a.chunk.js"
+  },
+  {
+    "revision": "d764ec9bdc86ba26aadb",
+    "url": "/productivity-app/static/js/main.d500b038.chunk.js"
+  },
+  {
+    "revision": "fb35b859eca272a45bb3",
+    "url": "/productivity-app/static/js/runtime~main.5d568d19.js"
+  }
+]);
